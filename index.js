a8d0cb06d2db21a00d6e840574e606dc00d32d89
AdityaSingh772 <workwithracian@gmail.com>
2024-05-20 19:58:01 +0530

    updated env and comment

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+require('dotenv').config();
+
 const express = require("express");
 const path = require("path");
 const userRouter = require("./routes/user");
-
-//connection
-
-
+const mongoose = require("mongoose");
 const app = express();
 
+//connection
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("mongoDB connected");
+})
+
+
+//middleWares
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
-
-
 app.use(express.urlencoded({extended:false}));
 
+//routes
 app.use('/', userRouter);
 
+
+//server start
 app.listen(process.env.PORT || 3000 , ()=>{
     console.log(`Server is started at 3000`);
 });
